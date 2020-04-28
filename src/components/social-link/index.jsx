0435cfb4fe3f394be2cbace7afacf81d81d96884
luzzif <federico.luzzi@mondora.com>
2020-04-28 23:31:46 +0200

    chore: remove unused import

--- src/components/social-link/index.jsx
+++ src/components/social-link/index.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import PropTypes from "prop-types";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faFacebook, faInstagram } from "@fortawesome/free-brands-svg-icons";
-import { Link, RootFlex } from "./styled";
+import { RootFlex } from "./styled";
 import { Box } from "reflexbox";
 import { UndecoratedLink } from "../undecorated-link";
 
