3608ce82101e016dcbaace342932d6446b82fe05
luzzif <federico.luzzi@mondora.com>
2020-04-28 16:45:46 +0200

    feat: try out new full relation widget

--- src/cms/index.js
+++ src/cms/index.js
@@ -1,4 +1,6 @@
 import CMS from "netlify-cms-core";
 import { it } from "netlify-cms-locales";
+import { FullRelationControl } from "./widgets/full-relation";
 
 CMS.registerLocale("it", it);
+CMS.registerWidget("full-relation", FullRelationControl);
