497d65fd35f92db342337c7d9216b56dbdf1955a
luzzif <federico.luzzi@mondora.com>
2020-04-28 18:20:12 +0200

    feat: remove attributes collection

--- gatsby-config.js
+++ gatsby-config.js
@@ -4,7 +4,7 @@ module.exports = {
         description:
             "Lo shop online di Morbegnoprint ti da accesso alla quintessenza dei prodotti di stampa digitale da noi prodotti, con la solita e garantita professionalità, passione e qualità.",
         author: "@luzzif",
-        siteUrl: "https://www.shop.morbegnoprint.it"
+        siteUrl: "https://www.shop.morbegnoprint.it",
     },
     plugins: [
         "gatsby-plugin-react-helmet",
@@ -12,39 +12,33 @@ module.exports = {
             resolve: "gatsby-source-filesystem",
             options: {
                 name: "images",
-                path: `${__dirname}/src/images`
-            }
+                path: `${__dirname}/src/images`,
+            },
         },
         {
             resolve: "gatsby-source-filesystem",
             options: {
                 name: "uploads",
-                path: `${__dirname}/static/images/uploads`
-            }
+                path: `${__dirname}/static/images/uploads`,
+            },
         },
         {
             resolve: "gatsby-source-filesystem",
             options: {
                 path: `${__dirname}/src/pages`,
-                name: "pages"
-            }
+                name: "pages",
+            },
         },
         "gatsby-transformer-sharp",
         "gatsby-plugin-sharp",
         {
             resolve: "gatsby-plugin-typography",
             options: {
-                pathToConfigModule: "src/utils/typography"
-            }
+                pathToConfigModule: "src/utils/typography",
+            },
         },
         "gatsby-plugin-styled-components",
         "gatsby-plugin-sitemap",
-        {
-            resolve: "gatsby-plugin-netlify-cms",
-            options: {
-                modulePath: `${__dirname}/src/cms`
-            }
-        },
         {
             resolve: "gatsby-transformer-remark",
             options: {
@@ -52,21 +46,27 @@ module.exports = {
                     {
                         resolve: "gatsby-remark-relative-images",
                         options: {
-                            name: "uploads"
-                        }
+                            name: "uploads",
+                        },
                     },
                     {
                         resolve: "gatsby-remark-images",
-                        options: { maxWidth: 2048 }
+                        options: { maxWidth: 2048 },
                     },
                     {
                         resolve: "gatsby-remark-copy-linked-files",
                         options: {
-                            destinationDir: "static"
-                        }
-                    }
-                ]
-            }
-        }
-    ]
+                            destinationDir: "static",
+                        },
+                    },
+                ],
+            },
+        },
+        {
+            resolve: "gatsby-plugin-netlify-cms",
+            options: {
+                modulePath: `${__dirname}/src/cms`,
+            },
+        },
+    ],
 };
--- src/cms/index.js
+++ src/cms/index.js
@@ -1,7 +1,4 @@
 import CMS from "netlify-cms-core";
 import { it } from "netlify-cms-locales";
-import { FullRelationControl } from "./widgets/full-relation";
 
 CMS.registerLocale("it", it);
-CMS.registerWidget("full-relation", FullRelationControl);
-CMS.init();
