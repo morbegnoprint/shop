5f749bb88ac2af0f02d67a5b3712a8e6b9d3dcdb
luzzif <federico.luzzi@mondora.com>
2020-05-12 18:18:22 +0200

    fix: fix category name line height

--- src/components/categories/category/index.jsx
+++ src/components/categories/category/index.jsx
@@ -13,7 +13,12 @@ export const Category = ({ slug, name, image }) => (
         <RootFlex alignItems="flex-end">
             <BackgroundImage fluid={image.childImageSharp.fluid} />
             <Overlay />
-            <NameBox fontSize={24} fontWeight={700} color="#fff">
+            <NameBox
+                fontSize={24}
+                lineHeight="28px"
+                fontWeight={700}
+                color="#fff"
+            >
                 {name}
             </NameBox>
         </RootFlex>
