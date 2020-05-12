4717a4cd5eca4400a8e96e343a4ff8bfce4066e2
luzzif <federico.luzzi@mondora.com>
2020-05-12 17:52:12 +0200

    fix: add keys in gatsby ssr

--- gatsby-ssr.js
+++ gatsby-ssr.js
@@ -3,11 +3,15 @@ import React from "react";
 export const onRenderBody = ({ setPostBodyComponents }) => {
     setPostBodyComponents([
         <div
+            key="snipcart-container"
             hidden
             id="snipcart"
             data-api-key="MTcyMDQxNzUtY2Q4ZS00MmNhLTk2MTEtZjY3MTM1MjFjZDRkNjM3MjQwMTAyODQ4MTE2ODE3"
             data-currency="EUR"
         />,
-        <script src="https://cdn.snipcart.com/themes/v3.0.12/default/snipcart.js" />,
+        <script
+            key="snipcart-js"
+            src="https://cdn.snipcart.com/themes/v3.0.12/default/snipcart.js"
+        />,
     ]);
 };
